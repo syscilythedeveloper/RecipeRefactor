be433c43f528146e771fcfe7678f4381c950b9ed
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2024-01-04 22:02:40 -0500

    installing splide

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import styled from "styled-components";
-
+import {Splide, SplideSlide} from "@splidejs/react-splide";
+import '@splidejs/splide/dist/css/splide.min.css';
 
 function Popular() {
 
