dc8bc22524bd1f77a14057decf38b1b8d07c9417
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2024-01-06 14:49:06 -0500

    Adding active class to cuisine

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -8,27 +8,27 @@ import { NavLink } from 'react-router-dom';
 
 const Category = () => {
   return (
-    <List>
+    <List activeClassName="activeClass">
 
-      <NavLink to={'/cuisine/Italian'}>
+      <SLink to={'/cuisine/Italian'}>
         <FaPizzaSlice />
         <h4>Italian</h4>
-      </NavLink>
+      </SLink>
 
-      <NavLink to={'/cuisine/American'}>
+      <SLink to={'/cuisine/American'}>
         <FaHamburger />
         <h4>American</h4>
-      </NavLink>
+      </SLink>
 
-      <NavLink to={'/cuisine/Thai'}>
+      <SLink to={'/cuisine/Thai'}>
         <GiNoodles />
         <h4>Thai</h4>
-      </NavLink>
+      </SLink>
 
-      <NavLink to={'/cuisine/Japanese'} >
+      <SLink to={'/cuisine/Japanese'} >
         <GiChopsticks/>
-        <h4>Chopsticks</h4>
-      </NavLink>
+        <h4>Japanese</h4>
+      </SLink>
     </List>
   )
 }
@@ -36,5 +36,41 @@ const Category = () => {
 const List = styled.div`
 display:flex;
 justify-content: center;
-margin: 2rem 0rem;`
+margin: 2rem 0rem;
+`
+
+const SLink = styled(NavLink)`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+border-radius: 50%;
+margin-right: 2rem;
+text-decoration: none;
+background: linear-gradient(35deg, #494949, #313131);
+width: 6rem;
+height:6rem;
+transform: scale(0.8);
+
+h4{
+    color: white;
+    font-size: 0.8rem;
+}
+
+svg{
+    color: white;
+    font-size: 1.5rem;
+}
+
+
+&.active, activeClass {
+    background: linear-gradient(to right, #f27121, #e94057);
+    svg {
+        color: white;
+    }
+    h4 {
+        color: white;
+    }
+`;
+
 export default Category
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -6,7 +6,8 @@ import {Link, useParams} from 'react-router-dom'
 
 const Cuisine = () => {
 
-    const[cuisine, setCuisine] = useState([])
+    const[cuisine, setCuisine] = useState([]);
+
     let params = useParams()
 
     const getCuisine = async(name) => {
@@ -19,46 +20,48 @@ const Cuisine = () => {
         getCuisine(params.type)
         console.log(params.type)
 
-    }, [params.type])
+    }, [params.type]);
   return (
-    <Grid>
-        {cuisine.map((item ) => {
-            return (
-                <Card key = {item.id}> 
-                    <img src={item.image} alt ="" />
-                    <h4>{item.title}</h4>
+   <Grid>
+    {cuisine.map((item) =>{
 
-                </Card>
-            )
-        })}
-      
-    </Grid>
-  )
+        return (
+            <Card key = {item.id}>
+                <img src={item.image} alt="" />
+                <h4> {item.title} </h4>
+
+            </Card>
+        )
+    }
+    
+    )}
+
+    </ Grid>
+    );
 }
 
 const Grid = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+    grid-template-columns: repeat(4, minmax(250px, 1fr) );
     grid-gap: 3rem;
-    
 `
 
-const Card = styled.div`
-   
-    img{
+const Card = styled.div ` 
+    img {
         width: 100%;
-        border-radius: 2rem;
-    }
-    
-    a{
-        text-decoration: none
+        border-radius: 2rem
     }
 
-    h4{
-        text-align: center; 
+    a {
+        text-decoration: none;
+    }
+
+    h4 {
+        text-align: center;
         padding: 1rem;
     }
-    
 `
+      
+
 
 export default Cuisine
