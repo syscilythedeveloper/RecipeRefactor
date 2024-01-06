2534993bf55a2d0fbda7153479d28a22b3d8eb7f
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2024-01-06 14:52:00 -0500

    Adding Search Component

---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useState } from 'react'
+import { FaSearch } from 'react-icons/fa'
+
+const Search = () => {
+  return (
+    <form>
+        <input type="text" />
+    </form>
+  )
+}
+
+export default Search
