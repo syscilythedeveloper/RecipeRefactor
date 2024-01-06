e0100c4d0ac70e837df9f6ef46be29a34fea69ce
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2024-01-06 14:03:58 -0500

    Adding Categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
      
       <Category />
       <Pages />
+      </BrowserRouter>
     
 
     
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,6 +1,7 @@
 import {FaPizzaSlice, FaHamburger} from 'react-icons/fa';
 import {GiNoodles, GiChopsticks} from 'react-icons/gi';
 import styled from "styled-components";
+import { NavLink } from 'react-router-dom';
 
 
 
@@ -9,25 +10,25 @@ const Category = () => {
   return (
     <List>
 
-      <div>
+      <NavLink to={'/cuisine/Italian'}>
         <FaPizzaSlice />
         <h4>Italian</h4>
-      </div>
+      </NavLink>
 
-      <div>
+      <NavLink to={'/cuisine/American'}>
         <FaHamburger />
         <h4>American</h4>
-      </div>
+      </NavLink>
 
-      <div>
+      <NavLink to={'/cuisine/Thai'}>
         <GiNoodles />
         <h4>Thai</h4>
-      </div>
+      </NavLink>
 
-      <div>
-        <GiChopsticks />
+      <NavLink to={'/cuisine/Japanese'} >
+        <GiChopsticks/>
         <h4>Chopsticks</h4>
-      </div>
+      </NavLink>
     </List>
   )
 }
