817b34b423eeb23060e2f7e55dbd2dbc68bf7a61
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2024-01-09 06:37:43 -0500

    Adding search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Search />
      
       <Category />
       <Pages />
