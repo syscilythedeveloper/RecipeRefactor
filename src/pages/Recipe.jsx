0a7afe75537e3e2b62e99ceca31dba3ddc8d881d
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2024-01-09 11:17:41 -0500

    Addings ingredient and instruction buttons with styling

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -24,10 +24,49 @@ function Recipe() {
 
 
   return (
-    <div>
-      {details.title}
-    </div>
+    <DetailWrapper>
+      <div>
+        <h2>{details.title}</h2>
+        <img src ={details.image} alt=""></img>
+      </div>
+      <Info>
+        <Button> Instructions </Button>
+        <Button> Ingredients </Button>
+      
+      </Info>
+    </DetailWrapper>
   )
 }
 
+const DetailWrapper = styled.div`
+    margin-top: 10rem;
+    margin-bottom: 5rem;;
+    display: flex; 
+    .active {
+        background: linear-gradient( #494949, #313131);
+        color: white;
+    }
+    h2{ 
+        margin-bottom: rem;
+        }
+    li{ font-size: 1.2rem;
+        line-height: 2.5rem;
+    }
+    ul {
+        margin-top: 2rem;
+    };
+`
+
+const Button = styled.button`
+    padding: 1rem 2rem;
+    color: #313131;
+    background: white;
+    border: 2px solid black;
+    margin-right: 2rem;
+    font-weight: 600;
+`
+
+const Info = styled.div`
+margin-left: 2rem;
+`
 export default Recipe
