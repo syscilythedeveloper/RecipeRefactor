aa82c37a004f9637134d819ca0413edcc2f3a184
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2024-01-09 17:56:03 -0500

    Revert "Adding in transitions"
    This reverts commit f03c4dc19fe368006a5462ccb5fcb8ef8beb5dee.

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -3,24 +3,19 @@ import Home from './Home'
 import Cuisine from './Cuisine'
 import Searched from './Searched'
 import Recipe from './Recipe'
-import {Route, Routes, useLocation} from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion'
+import {Route, Routes} from 'react-router-dom'
 
 const Pages = () => {
-  const location = useLocation();
   return (
     
-    
-      <AnimatePresence mode="wait">
-        <Routes location ={location} key ={location.pathname}>
-          
-            <Route  path="/" element={<Home />} />
-            <Route  path="/cuisine/:type" element={<Cuisine/>} />
-            <Route  path="/searched/:search" element={<Searched />} />
-            <Route  path="/recipe/:name" element={<Recipe />} />
-          
-        </Routes>
-          </AnimatePresence>
+      <Routes>
+        
+          <Route  path="/" element={<Home />} />
+          <Route  path="/cuisine/:type" element={<Cuisine/>} />
+          <Route  path="/searched/:search" element={<Searched />} />
+          <Route  path="/recipe/:name" element={<Recipe />} />
+        
+      </Routes>
     
   )
 }
