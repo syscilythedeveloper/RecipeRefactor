76a984715eef17c75eb444b4c7f04650b58f0f0a
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2024-01-16 10:33:38 -0500

    Include nutrition info

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -11,7 +11,7 @@ function Recipe() {
     const [activeTab, setActiveTab] = useState('instructions')
     
     const fetchDetails = async () => {
-        const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`);
+        const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=true`);
         const detailData = await data.json()
         setDetails(detailData)
         console.log(detailData)
