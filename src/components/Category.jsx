5ed504738a746d812e2f514efd70b1863dda88af
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2024-01-26 05:36:02 -0500

    Adding heading for cuisines

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,109 +1,96 @@
-import {FaHamburger} from 'react-icons/fa';
-import {GiChiliPepper,  GiRoastChicken, GiTacos } from 'react-icons/gi';
+import { FaHamburger } from "react-icons/fa";
+import { GiChiliPepper, GiRoastChicken, GiTacos } from "react-icons/gi";
 import styled from "styled-components";
-import { NavLink } from 'react-router-dom';
-
-
-
+import { NavLink } from "react-router-dom";
 
 const Category = () => {
   return (
-    <List activeClassName="activeClass">
+    <>
+      <Heading>Buzzworthy Bites</Heading>
+      <List activeClassName="activeClass">
+        <SLink to={"/cuisine/Caribbean"}>
+          <GiRoastChicken />
+          <p>Caribbean</p>
+        </SLink>
 
-    
-      <SLink to={'/cuisine/Caribbean'}>
-        <GiRoastChicken />
-        <p>Caribbean</p>
-      </SLink>
+        <SLink to={"/cuisine/Mexican"}>
+          <GiTacos />
+          <p>Mexican</p>
+        </SLink>
 
-      <SLink to={'/cuisine/Mexican'}>
-        <GiTacos />
-        <p>Mexican</p>
-      </SLink>
+        <SLink to={"/cuisine/American"}>
+          <FaHamburger />
+          <p>American</p>
+        </SLink>
 
+        <SLink to={"/cuisine/Thai"}>
+          <GiChiliPepper />
+          <p>Thai</p>
+        </SLink>
+      </List>
+    </>
+  );
+};
 
-      <SLink to={'/cuisine/American'} >
-        <FaHamburger/>
-        <p>American</p>
-      </SLink>
-
-      <SLink to={'/cuisine/Thai'} >
-        <GiChiliPepper/>
-        <p>Thai</p>
-      </SLink>
-    
-    
-
-
-    </List>
-  )
-}
+const Heading = styled.h4`
+  // Add your styles for h4 here
+  color: #482908; // Example color
+  text-align: center;
+  font-size: 0.8em;
+`;
 
 const List = styled.div`
-display:flex;
-justify-content: center;
-margin: 2rem 15rem;
+  display: flex;
+  justify-content: center;
+  margin: 1rem 15rem;
 
-@media screen and (max-width: 450px) {
-  align-items: center;
-  display:inline-block
- 
-
-
-  p{
-    font-size: 0.65rem
+  @media screen and (max-width: 450px) {
+    align-items: center;
+    display:inline-block p {
+      font-size: 0.65rem;
+    }
   }
-  
-}
-
-
-`
+`;
 
 const SLink = styled(NavLink)`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-border-radius: 50%;
-margin-right: 2rem;
-text-decoration: none;
-background: linear-gradient(35deg, #494949, #313131);
-width: 6rem;
-height:6rem;
-transform: scale(0.8);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-right: 2rem;
+  text-decoration: none;
+  background: linear-gradient(35deg, #494949, #313131);
+  width: 6rem;
+  height: 6rem;
+  transform: scale(0.8);
 
-p{
+  p {
     color: white;
     font-size: 1rem;
-}
+  }
 
-
-svg{
+  svg {
     color: white;
     font-size: 1.5rem;
-}
+  }
 
-
-&.active {
-    background: #FFD68A;
+  &.active {
+    background: #ffd68a;
     box-shadow: 0px 0px 10px 5px #482908;
     transform: scale(1.05);
 
-
-
     svg {
-        color: #482908;
-        
+      color: #482908;
     }
     p {
-        color: #482908;
+      color: #482908;
     }
-
   }
 
-&:hover {
+  &:hover {
     box-shadow: 0px 0px 15px 5px black;
-  } 
+  }
 `;
 
-export default Category
+export default Category;
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,93 +1,79 @@
-import { motion } from 'framer-motion'
-import {React, useEffect, useState} from 'react'
-import styled from 'styled-components'
-import {Link, useParams} from 'react-router-dom'
-
-
+import { motion } from "framer-motion";
+import { React, useEffect, useState } from "react";
+import styled from "styled-components";
+import { Link, useParams } from "react-router-dom";
 
 const Cuisine = () => {
+  const [cuisine, setCuisine] = useState([]);
 
-    const[cuisine, setCuisine] = useState([]);
+  let params = useParams();
 
-    let params = useParams()
-
-    const getCuisine = async(name) => {
-        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`)
-        const recipes = await data.json();
-        setCuisine(recipes.results)
-    }
-
-    useEffect(() => {
-        getCuisine(params.type)
-        console.log(params.type)
-
-    }, [params.type]);
-  return (
-   <Grid
-        animate={{opacity: 1}}
-        initial = {{opacity:0}}
-        exit={{opacity: 0}}
-        transition={{duration: 0.5}}
-   
-   >
-    {cuisine.map((item) =>{
-
-        return (
-            <Card key = {item.id}>
-                <Link to = {"/recipe/" + item.id}> 
-                    <img src={item.image} alt="" />
-                    <p> {item.title} </p>
-                </Link>
-
-            </Card>
-        )
-    }
-    
-    )}
-
-    </ Grid>
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
     );
-}
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+
+  useEffect(() => {
+    getCuisine(params.type);
+    console.log(params.type);
+  }, [params.type]);
+  return (
+    <Grid
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      {cuisine.map((item) => {
+        return (
+          <Card key={item.id}>
+            <Link to={"/recipe/" + item.id}>
+              <img src={item.image} alt="" />
+              <p> {item.title} </p>
+            </Link>
+          </Card>
+        );
+      })}
+    </Grid>
+  );
+};
 
 const Grid = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(4, minmax(250px, 1fr));
+  grid-gap: 3rem;
+
+  @media screen and (max-width: 450px) {
     display: grid;
-    grid-template-columns: repeat(4, minmax(250px, 1fr) );
-    grid-gap: 3rem;
-    
-
-    @media screen and (max-width: 450px) {
-        display: grid;
-        grid-template-columns: repeat(2, minmax(150px, 1fr) );
-        grid-gap: 1.5rem;
-
-        p{
-            font-size: 0.5;
-        }
-        margin:2px;
-
-    }
-`
-
-const Card = styled.div ` 
-    img {
-        width: 100%;
-        border-radius: 2rem
-    }
-
-    a {
-        text-decoration: none;
-    }
+    grid-template-columns: repeat(2, minmax(150px, 1fr));
+    grid-gap: 1.5rem;
 
     p {
-        text-align: center;
-        padding: 1rem;
-        font-weight: 600;
-        font-size: 1rem;
+      font-size: 0.5;
     }
+    margin: 2px;
+  }
+`;
 
-    
-`
-      
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
 
+  a {
+    text-decoration: none;
+  }
 
-export default Cuisine
+  p {
+    text-align: center;
+    padding: 1rem;
+    font-weight: 600;
+    font-size: 1rem;
+  }
+`;
+
+export default Cuisine;
