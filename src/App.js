5bb249503f50fc3521407e99ab5daeb3f34bc706
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2024-01-26 06:03:55 -0500

    Adding descriptions

--- src/App.js
+++ src/App.js
@@ -6,52 +6,62 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { GiKnifeFork } from "react-icons/gi";
 
-import PopUp from "./components/Popup"
-
+import PopUp from "./components/Popup";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Nav>
-        <GiKnifeFork />
-        <Logo to={"/"}> NutriRecipe</Logo>
-      </Nav>
-      
-      <Search />
-      <PopUp />
-      
-     
-      <Category />
-      <Pages />
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}> NutriRecipe</Logo>
+        </Nav>
+
+        <WelcomeMessage>
+          <p>
+            {" "}
+            Welcome, Chef! Get ready to embark on a culinary adventure where we
+            cater to all your recipe needs. Explore the delectable world of
+            Whole30, discover our Popular Picks, and indulge in the latest
+            Buzzworthy Bites below. Don't forget to dive into other tantalizing
+            recipes using our search bar and extended search. Let's start your
+            culinary journey!
+          </p>
+        </WelcomeMessage>
+
+        <Search />
+        <PopUp />
+
+        <Category />
+        <Pages />
       </BrowserRouter>
-    
-
-    
-
     </div>
   );
 }
 
 const Logo = styled(Link)`
   text-decoration: none;
-  font-size: 1.5rem; 
+  font-size: 1.5rem;
   font-weight: 400;
   font-family: "Lobster Two", cursive;
-  color:#B5651D;
-  
-`
+  color: #b5651d;
+`;
 
 const Nav = styled.div`
-padding: 4rem 0rem;
-display: flex;
-justify-content: flex-start;
-align-items: center;
-svg{
-  font-size: 1.5rem;
-  color:#B5651D;
-}
+  padding: 1rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 1.5rem;
+    color: #b5651d;
+  }
+`;
 
-`
+const WelcomeMessage = styled.div`
+  width: 50%;
+  font-size: 0.8rem;
+  color: #b5651d;
+`;
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,19 @@
 import Whole30 from "../components/Whole30";
 import Popular from "../components/Popular";
-import {motion} from 'framer-motion'
-
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <motion.div
-      animate={{opacity: 1}}
-      initial = {{opacity:0}}
-      exit={{opacity: 0}}
-      transition={{duration: 0.5}}
-    
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
     >
-        <Whole30 />
-        <Popular />
-      
+      <Whole30 />
+      <Popular />
     </motion.div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
